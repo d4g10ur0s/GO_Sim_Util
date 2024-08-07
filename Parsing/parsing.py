de48bc8b6e1080a6a79ab2cc1f072c5425146e20
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-08-07 15:20:45 +0300

    Add files via upload

--- Parsing/parsing.py
+++ Parsing/parsing.py
@@ -1,4 +1,5 @@
 import sys
+import json
 # data analysis tools
 import pandas as pd
 # graph analysis tools
@@ -6,16 +7,50 @@ import networkx as nx
 # ontology analysis tools
 import ontobio as ob
 # parallel programming
-from dask.distributed import Client,LocalCluster
+from dask.distributed import Client,LocalCluster,progress
 import dask
 
-def retPath(ont ,G , root , n,counter):
-    counter+=1
+def save_to_json(gene_dict, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(gene_dict, f, indent=4)
+
+def parseGAF(ga_path):
+    '''
+    Input : Path for .gaf file .
+    - - - - -
+    Process : Create a dictionary of DB:id as keys with values [(GO:id , evidence), ... , (GO:id, evidence)]
+    - - - - -
+    Output : dict of genes with their corresponding go terms and evidence for annotation
+    '''
+    print('Reading GAF .')
+    print(str(ga_path))
+    geneDict = {}
+    gap = ob.io.gafparser.GafParser()
+    with open(ga_path , 'r+') as gaf :
+        line = gaf.readline()
+        counter = 1
+        while line :
+            try : # first lines do not have useful information
+                line = gaf.readline()
+                gline = gap.parse_line(line)
+                g = gline.associations
+                for asc in g :
+                    if not (asc.subject.id.namespace+':'+asc.subject.id.identity) in geneDict.keys():
+                        geneDict[asc.subject.id.namespace+':'+asc.subject.id.identity] = []
+                    geneDict[asc.subject.id.namespace+':'+asc.subject.id.identity].append((asc.object.id.namespace+asc.object.id.identity , str(asc.evidence.gaf_evidence_code())))
+            except (AttributeError,TypeError) as e :
+                print(str(e))
+            finally :
+                counter+=1
+                if counter%10000 == 0:
+                    print(f'Terms processed : {counter}')
+        #endwhile
+        return geneDict
+
+'''
+def retPath(ont ,G , root , n ):
     paths = list(nx.all_simple_paths(G, root, n))
     longest = len(max(paths, key=lambda p: len(p)))
-    # some info
-    if counter % 100 == 0:
-        print(f"Processed {counter} terms...")
     return {'id':n,
             'label': ont.label(n),
             'pathcount': len(paths),
@@ -26,23 +61,34 @@ def get_pathstats(ont ,G , root , nodes):
     for any given node, return a table row with stats
     """
     # parallel programming client
-    cluster = LocalCluster(processes=False, threads_per_worker=8)
-    client = Client(cluster)
+    #cluster = LocalCluster(processes=False, threads_per_worker=2)
+    #client = Client(cluster)
     items = []
     counter=0
-    futures = [client.submit(retPath,ont ,G , root , n,counter) for n in nodes]
-    progress(futures)
-    return futures
+    for n in nodes:
+        counter+=1
+        items.append(retPath(ont ,G , root , n))
+        if counter%10 == 0:
+            print(f'Terms processed : {counter}')
+    #progress(futures)
+    return items
+'''
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <obo_file_path>")
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <obo_file_path> <ga_file_path>")
         sys.exit(1)
     # path for .obo
     obo_path = sys.argv[1]
+    # path for .gaf
+    ga_path = sys.argv[2]
     # create ontology
-    print('Creating Ontology from file .')
-    ont = ob.OntologyFactory().create(obo_path)
+    #print('Creating Ontology from file .')
+    #ont = ob.OntologyFactory().create(obo_path)
+    geneDict = parseGAF(ga_path)
+    save_to_json(geneDict,"./Datasets/genes.json")
+    exit(0)
+    '''
     # get root nodes
     print('Getting Root Nodes !')
     rootNodes=[]
@@ -51,6 +97,7 @@ def main():
     # get ontology graph
     print('Creating Ontology Graph !')
     G = ont.get_graph()
+    print(str(len(G)))
     # get different subontologies
     print('Descendants for : ' + ont.label(rootNodes[1]))
     cc = list(ont.descendants(rootNodes[1]))
@@ -73,6 +120,7 @@ def main():
     print('Stats for MF !')
     mf_df = get_pathstats(ont , G , rootNodes[3] , mf)
     print(mf_df)
+    '''
 
 if __name__ == "__main__":
     main()
